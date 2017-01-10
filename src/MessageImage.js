e80502496e54269c46c73ac36b33ca0bf1115a60
Kevin Cooper <cooperka92@gmail.com>
2017-01-10 11:39:29 -0500

    Pass message data to 'onLoad' callback

--- src/MessageImage.js
+++ src/MessageImage.js
@@ -11,6 +11,8 @@ export default class MessageImage extends React.Component {
   render() {
     const { width, height } = Dimensions.get('window');
 
+    const { onLoad, onError } = this.props.imageProps;
+
     return (
       <View style={[styles.container, this.props.containerStyle]}>
         <Lightbox
@@ -21,6 +23,8 @@ export default class MessageImage extends React.Component {
         >
           <Image
             {...this.props.imageProps}
+            onLoad={onLoad ? () => onLoad(this.props.currentMessage) : undefined}
+            onError={onError ? () => onError(this.props.currentMessage) : undefined}
             style={[styles.image, this.props.imageStyle]}
             source={{uri: this.props.currentMessage.image}}
           />
